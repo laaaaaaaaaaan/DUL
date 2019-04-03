14659396e3705f27d620ff7d7030271196f6ca55
IN1DM3B_16 <IN1DM3B_16@H015112.NET>
2019-04-03 14:18:28 +0200

    asasd

--- DUL/src/main/java/modelo/hotel.java
+++ DUL/src/main/java/modelo/hotel.java
@@ -1,24 +1,5 @@
 package modelo;
 
-public class Hotel extends Alojamiento{
-	private int nEstrellas;
+public class Hotel {
 
-	public Hotel(String nombreAloj, String ubicacion, int numHabitaciones, int nEstrellas) {
-		super();
-		this.nombreAloj = nombreAloj;
-		this.ubicacion = ubicacion;
-		this.numHabitaciones = numHabitaciones;
-		this.nEstrellas = nEstrellas;
-	}
-
-	
-	public int getnEstrellas() {
-		return nEstrellas;
-	}
-
-	public void setnEstrellas(int nEstrellas) {
-		this.nEstrellas = nEstrellas;
-	}
-	
-	
 }
