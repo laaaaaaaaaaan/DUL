b245030cf79cd483fe9795f6d89523748914aabb
IN1DM3B_18 <IN1DM3B_18@H015112.NET>
2019-04-04 11:58:45 +0200

    merge conflict

--- DUL/src/main/java/interfaces/PanelBienvenida.java
+++ DUL/src/main/java/interfaces/PanelBienvenida.java
@@ -1,9 +1,13 @@
 package interfaces;
 
 import javax.swing.JPanel;
+
+import java.awt.Color;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class PanelBienvenida extends JPanel {
 
@@ -11,17 +15,18 @@ public class PanelBienvenida extends JPanel {
 	public JButton btnContinuar;
 	
 	public PanelBienvenida() {
-		
+		setBackground(new Color(255, 201, 164));
 		setBounds(0, 0, 800, 600);
 		setLayout(null);
 		
-		btnContinuar = new JButton("Continuar");
-		btnContinuar.setBounds(345, 496, 89, 23);
+		btnContinuar = new JButton("Hacer Reserva");
+		btnContinuar.setBounds(193, 496, 395, 23);
 		add(btnContinuar);
 		
 		lblBienvenido = new JLabel("Bienvenido");
+		lblBienvenido.setFont(new Font("Yu Gothic UI", Font.PLAIN, 34));
 		lblBienvenido.setHorizontalAlignment(SwingConstants.CENTER);
-		lblBienvenido.setBounds(303, 275, 163, 37);
+		lblBienvenido.setBounds(182, 59, 411, 103);
 		add(lblBienvenido);
 
 	}
--- DUL/src/main/java/interfaces/PanelHoteles.java
+++ DUL/src/main/java/interfaces/PanelHoteles.java
@@ -3,26 +3,34 @@ package interfaces;
 import javax.swing.JPanel;
 
 import javax.swing.JList;
+
+import java.awt.Color;
+
 import javax.swing.JButton;
 
 public class PanelHoteles extends JPanel {
 
-	public JList hoteles;
+	public JList hoteles, infohoteles;
 	public JButton btnAtras;
 	
 	public PanelHoteles() {
-
+		setBackground(new Color(255, 201, 164));
 		setBounds(0, 0, 800, 600);
 		setLayout(null);
 		
+		btnAtras = new JButton("Atras");
+		btnAtras.setBounds(27, 536, 89, 23);
+		add(btnAtras);
+		
 		
 		hoteles = new JList();
-		hoteles.setBounds(27, 163, 280, 143);
+		hoteles.setBounds(27, 189, 194, 181);
 		add(hoteles);
 		
-		btnAtras = new JButton("Atras");
-		btnAtras.setBounds(596, 510, 89, 23);
-		add(btnAtras);
+		
+		infohoteles = new JList();
+		infohoteles.setBounds(410, 189, 323, 317);
+		add(infohoteles);
 		
 		
 		
