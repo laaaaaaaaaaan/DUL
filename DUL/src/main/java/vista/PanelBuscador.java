e008991fd6d3c9f52f49b998ca2151f74bf508b3
IN1DM3B_18 <IN1DM3B_18@H015112.NET>
2019-04-05 12:40:03 +0200

    merge conflict

--- DUL/src/main/java/vista/PanelBuscador.java
+++ DUL/src/main/java/vista/PanelBuscador.java
@@ -9,6 +9,7 @@ import javax.swing.JLabel;
 
 public class PanelBuscador extends JPanel {
 	public JTextField textFieldUbicacion;
+	public JLabel lblUbicacion;
 	public JButton btnContinuar;
 	
 	public PanelBuscador() {
@@ -16,7 +17,7 @@ public class PanelBuscador extends JPanel {
 		setBounds(0, 0, 800, 600);
 		setLayout(null);
 		
-		JLabel lblUbicacion = new JLabel("Ubicacion");
+		lblUbicacion = new JLabel("Ubicacion");
 		lblUbicacion.setBounds(64, 158, 101, 23);
 		add(lblUbicacion);
 		
@@ -25,7 +26,7 @@ public class PanelBuscador extends JPanel {
 		textFieldUbicacion.setBounds(202, 158, 182, 23);
 		add(textFieldUbicacion);
 		
-		JButton btnContinuar = new JButton("Continuar");
+		btnContinuar = new JButton("Continuar");
 		btnContinuar.setBounds(149, 414, 475, 23);
 		add(btnContinuar);
 		
